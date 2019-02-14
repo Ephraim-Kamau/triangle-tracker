ab3fec4a72e06eec322d88423a3dd3395fb83635
Ephraim-Kamau <kephraim19@gmail.com>
2019-02-14 11:44:19 +0300

    Added triangle conditions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,18 @@
 function btn() {
-  var length1 = document.getElementById("length1");
-  var length2 = document.getElementById("length2");
-  var length3 = document.getElementById("length3");
+  var length1 = document.getElementById("hypotenus");
+  var length2 = document.getElementById("adjacent");
+  var length3 = document.getElementById("opposite");
 
-  var length4 = parseInt(length1);
-  var length5 = parseInt(length2);
-  var length6 = parseInt(length3);
-var add = length4 + length5 + length6;
+  var length4 = parseInt(hypotenus);
+  var length5 = parseInt(adjacent);
+  var length6 = parseInt(opposite);
 document.getElementById("answer").innerHTML=add;
+
+if (hypotenus === adjacent && hypotenus === opposite && adjacent === opposite) {
+  else if (hypotenus === adjacent || hypotenus === opposite || adjacent === opposite) {
+  };
+  else if (hypotenus !== adjacent || hypotenus !== opposite || adjacent !== opposite )
+  };
+else ((hypotenus + adjacent) <= opposite || (hypotenus + opposite) <= adjacent || (adjacent + opposite) <= hypotenus) {
+  };
 };
