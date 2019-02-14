6ce3b3264c709c469c3abf9cba3d7c025fd55026
Ephraim-Kamau <kephraim19@gmail.com>
2019-02-14 14:04:13 +0300

    Add changes to the variable names

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,21 @@
-function btn() {
-  var length1 = document.getElementById("hypotenus");
-  var length2 = document.getElementById("adjacent");
-  var length3 = document.getElementById("opposite");
+function button() {
+  var hypotenus = document.getElementById("hypotenus");
+  var adjacent = document.getElementById("adjacent");
+  var opposite = document.getElementById("opposite");
 
-  var length4 = parseInt(hypotenus);
-  var length5 = parseInt(adjacent);
-  var length6 = parseInt(opposite);
-document.getElementById("answer").innerHTML=add;
+  var length1 = parseInt(hypotenus);
+  var length2 = parseInt(adjacent);
+  var length3 = parseInt(opposite);
 
-if (hypotenus === adjacent && hypotenus === opposite && adjacent === opposite) {
-  else if (hypotenus === adjacent || hypotenus === opposite || adjacent === opposite) {
+
+if (length1 === length2 && length1 === length3 && length2 === length3){
+alert("This is an equilateral triangle")};
+  else if (length1 === length2 || length1 === length3 || length2 === length3) {
+    alert("This is an isosceles triangle");
   };
-  else if (hypotenus !== adjacent || hypotenus !== opposite || adjacent !== opposite )
+  else if (length1 !== length2 || length1 !== length3 || length2 !== length3 ) {
+    alert("This is a scalene triangle");
   };
-else ((hypotenus + adjacent) <= opposite || (hypotenus + opposite) <= adjacent || (adjacent + opposite) <= hypotenus) {
+else ((length1 + length2) <= length3 || (length1 + length3) <= length2 || (length2 + length3) <= hypotenus) {
+  alert("A triangle cannot be formed");
   };
-};
