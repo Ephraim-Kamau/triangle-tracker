6c6e059956906f133665e25912240bcde3250b08
Ephraim-Kamau <kephraim19@gmail.com>
2019-02-14 09:27:38 +0300

    Added forms to the html file

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+var length1 = parseInt(prompt("Enter the first length:"));
+var length2 = parseInt(prompt("Enter the second length:"));
+var length3 = parseInt(prompt("Enter the third length:"));
+alert("Wonderful");
