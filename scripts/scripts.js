d34ec92defbdbc77d2d4f9dad094606caa7a8fbb
Ephraim-Kamau <kephraim19@gmail.com>
2019-02-14 10:59:51 +0300

    Add header tags and js scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,11 @@
-var length1 = parseInt(prompt("Enter the first length:"));
-var length2 = parseInt(prompt("Enter the second length:"));
-var length3 = parseInt(prompt("Enter the third length:"));
-alert("Wonderful");
+function btn() {
+  var length1 = document.getElementById("length1");
+  var length2 = document.getElementById("length2");
+  var length3 = document.getElementById("length3");
+
+  var length4 = parseInt(length1);
+  var length5 = parseInt(length2);
+  var length6 = parseInt(length3);
+var add = length4 + length5 + length6;
+document.getElementById("answer").innerHTML=add;
+};
